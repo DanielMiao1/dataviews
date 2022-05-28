02c2264140c4496faa2981b12343bca9709b1e4b
Daniel M <danielmiao2019@icloud.com>
2022-05-28 10:19:08 -0700

    added top bar

--- main.py
+++ main.py
@@ -14,20 +14,50 @@ except ModuleNotFoundError:
 	except ModuleNotFoundError:
 		exit("The PyQt5 graphics library is not installed. Install it in the command-line using 'pip3 install PyQt5'.")
 
-import _thread
 import math
 
 
 class TopBar(QGroupBox):
-	class Button(QSvgWidget):
+	class CloseButton(QSvgWidget):
+		"""<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" width="2" height="2" fill="currentColor"><line x1="0.18" y1="0.18" x2="1.82" y2="1.82" style="stroke: rgb(0, 0, 0); stroke-width: 0.5"/><line x1="1.82" y1="0.18" x2="0.18" y2="1.82" style="stroke: rgb(0, 0, 0); stroke-width: 0.5"/></svg>"""
 		def __init__(self, parent):
-			super(TopBar.Button, self).__init__(parent=parent)
+			super(TopBar.CloseButton, self).__init__(parent=parent)
+			self.updateColor(QColor("black"))
+			self.setFixedSize(QSize(15, 15))
+			self.move(QPoint(5, 5))
+			self.animation = None
+			self.setCursor(Qt.CursorShape.PointingHandCursor)
+		
+		def enterEvent(self, event):
+			self.animation = QPropertyAnimation(self, b"color")
+			self.animation.setStartValue(QColor("black"))
+			self.animation.setEndValue(QColor("#383838"))
+			self.animation.setDuration(200)
+			self.animation.start()
+			super(TopBar.CloseButton, self).enterEvent(event)
+		
+		def leaveEvent(self, event) -> None:
+			self.animation = QPropertyAnimation(self, b"color")
+			self.animation.setStartValue(QColor("#383838"))
+			self.animation.setEndValue(QColor("black"))
+			self.animation.setDuration(200)
+			self.animation.start()
+			super(TopBar.CloseButton, self).leaveEvent(event)
+		
+		def mousePressEvent(self, event) -> None:
+			QApplication.quit()
+			super(TopBar.CloseButton, self).mousePressEvent(event)
+		
+		def updateColor(self, color: QColor):
+			self.renderer().load(bytearray(f'<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" width="2" height="2" fill="currentColor"><path style="stroke:{color.name()};stroke-width:.5" d="m.18.18 1.64 1.64m0-1.64L.18 1.82"/></svg>', encoding="utf-8"))
+		
+		color = pyqtProperty(QColor, fset=updateColor)
 
 	def __init__(self, parent):
 		super(TopBar, self).__init__(parent=parent)
 		self.setLayout(QHBoxLayout())
-		self.setStyleSheet("background-color: transparent;")
-		# self.close_button =
+		self.setStyleSheet("TopBar { background-color: rgba(255, 255, 255, 0.4); border-radius: 5px; }")
+		self.close_button = TopBar.CloseButton(self)
 
 
 class Widget(QPushButton):
@@ -66,26 +96,30 @@ class Clock(Widget):
 
 class Window(QMainWindow):
 	def __init__(self):
-		super(Window, self).__init__(flags=Qt.FramelessWindowHint | Qt.WindowTransparentForInput)
+		super(Window, self).__init__(flags=Qt.FramelessWindowHint)
 		self.setMinimumSize(QSize(200, 200))
 		self.setAttribute(Qt.WA_TranslucentBackground)
+		self.main_wrapper = QWidget(self)
 		self.top_bar = TopBar(self)
 		self.top_bar.hide()
-		self.clock = Clock(self)
+		self.clock = Clock(self.main_wrapper)
 		self.show()
 		self.leaveEvent = lambda event: super(Window, self).leaveEvent(event)
 	
 	def resizeEvent(self, event):
-		#
+		self.main_wrapper.resize(event.size())
+		self.top_bar.resize(QSize(event.size().width(), 25))
 		super(Window, self).resizeEvent(event)
 	
 	def enterEvent(self, event):
 		self.top_bar.show()
-		self.resize(QSize(self.width(), self.height() + self.top_bar.height()))
+		self.resize(QSize(self.width(), self.height() + self.top_bar.height() + 10))
+		self.main_wrapper.move(QPoint(0, self.top_bar.height() + 10))
 
 		def leaveEvent(event_):
 			self.top_bar.hide()
 			self.resize(QSize(self.width(), self.height() - self.top_bar.height()))
+			self.main_wrapper.move(QPoint(0, 0))
 			super(Window, self).leaveEvent(event_)
 
 		self.leaveEvent = leaveEvent
@@ -95,18 +129,4 @@ class Window(QMainWindow):
 application = QApplication([])
 bebas_neue = QFontDatabase.applicationFontFamilies(QFontDatabase.addApplicationFont(QDir.currentPath() + "/fonts/Bebas_Neue/BebasNeue-Regular.ttf"))[0]
 window = Window()
-
-
-print("Close the window by entering 'quit' in this console.")
-
-
-def check_input():
-	while True:
-		user_input = input()
-		if user_input.lower() == "quit":
-			QApplication.quit()
-			exit()
-
-	
-_thread.start_new_thread(check_input, ())
 application.exec_()
